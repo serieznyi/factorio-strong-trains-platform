4ce95536abb082d687b18bfca0feff47c91ce276
Maxim Mironyuk <serieznyi@gmail.com>
2023-05-03 07:59:04 +0300

    fix translation

--- src/settings.lua
+++ src/settings.lua
@@ -1,7 +1,7 @@
 data:extend({
     {
         type = "string-setting",
-        name = "stp-action-on-damaged-train",
+        name = "stp-action-on-damaged-trains",
         setting_type = "runtime-global",
         default_value = "1",
         allowed_values = {"1", "2", "3"}
