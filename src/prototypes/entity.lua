65d14afb086a994b4cd0d23fff61ee5b85724f94
Maxim Mironyuk <serieznyi@gmail.com>
2023-11-15 23:26:47 +0300

    Change smoke behavior for destroyed locomotive #14

--- src/prototypes/entity.lua
+++ src/prototypes/entity.lua
@@ -105,14 +105,14 @@ prototype.burner = {
     {
         {
             name = "train-smoke",
-            deviation = {0.3, 0.3},
-            frequency = 100,
-            position = {0, 0},
+            deviation = {0.0, 0.0},
+            frequency = 25,
+            position = {0.5, -3.0},
             starting_frame = 0,
             starting_frame_deviation = 60,
-            height = 2,
-            height_deviation = 0.5,
-            starting_vertical_speed = 0.2,
+            height = 0.15,
+            height_deviation = 0.1,
+            starting_vertical_speed = 0.05,
             starting_vertical_speed_deviation = 0.1
         }
     }
